25f66eebcfc120c751600ac2caaa060b6e35f967
Benjamin Jenks <jenksb@gmail.com>
2013-03-17 19:34:10 +0000

    indexer: start building a map of all song hashes

--- src/hzr/indexer.clj
+++ src/hzr/indexer.clj
@@ -4,18 +4,41 @@
             [hzr.fingerprint :as fingerprint])
   (:import [java.util Arrays]))
 
-(defn index-audio-stream [in]
+;; hash of fingerprint to list of songs
+(def fingerprint-index (atom {}))
+
+(defn fingerprint-audio-stream [in]
   (let [chunk-size 4096
         buffer (make-array Byte/TYPE chunk-size)]
-    (loop [bc (.read in buffer)]
-      (if-not (= bc -1)
-        (do
-          (println (fingerprint/fingerprint buffer))
-          (recur (.read in buffer)))))))
+    (loop [bc (.read in buffer)
+           fingerprints []]
+      (if (= bc -1)
+        fingerprints
+        (recur (.read in buffer) (conj fingerprints (fingerprint/fingerprint buffer)))))))
+
+(defn fingerprint-audio-file [file]
+  (audio/decode-audio-file file fingerprint-audio-stream))
+
+(defn load-audio-file-index [file]
+  (let [audio-file (io/file file)
+        index-file (io/file (str "index/" (.getName audio-file) ".idx"))]
+    (if (.exists index-file)
+      (read-string (slurp index-file))
+      (let [fprints (fingerprint-audio-file audio-file)
+            fingerprint-data {:filename (.getPath audio-file) :fingerprints fprints}]
+        (spit index-file fingerprint-data)
+        fingerprint-data))))
 
 (defn index-audio-file [file]
-  (audio/decode-audio-file file index-audio-stream))
+  (let [fingerprint-data (load-audio-file-index file)
+        filename (keyword (:filename fingerprint-data))
+        fprints (:fingerprints fingerprint-data)
+        index-data (map-indexed (fn [idx fp] {fp [idx filename]}) fprints)]
+    (println index-data)
+    ;(reset! fingerprint-index (merge-with concat @fingerprint-index
+    ;index-data))
+    ))
 
-(defn index [dir]
+(defn index-audio-dir [dir]
   (->> (filter #(.isFile %) (file-seq (io/file dir)))
        (map index-audio-file)))
