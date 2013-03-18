2814fb86790ba827ea8db0313e7f5ad120e70db0
Benjamin Jenks <jenksb@gmail.com>
2013-03-18 22:49:22 +0000

    indexer: added naive matching

--- src/hzr/indexer.clj
+++ src/hzr/indexer.clj
@@ -32,13 +32,34 @@
 (defn index-audio-file [file]
   (let [fingerprint-data (load-audio-file-index file)
         filename (keyword (:filename fingerprint-data))
-        fprints (:fingerprints fingerprint-data)
-        index-data (map-indexed (fn [idx fp] {fp [idx filename]}) fprints)]
-    (println index-data)
-    ;(reset! fingerprint-index (merge-with concat @fingerprint-index
-    ;index-data))
-    ))
+        fps (:fingerprints fingerprint-data)
+        index-data (map-indexed (fn [idx fp] {fp {idx filename}}) fps)]
+    (swap! fingerprint-index #(apply merge-with conj % index-data))))
 
 (defn index-audio-dir [dir]
   (->> (filter #(.isFile %) (file-seq (io/file dir)))
        (map index-audio-file)))
+
+(defn map-count [map key]
+  (assoc map key (inc (get map key 0))))
+
+(defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
+
+(defn match-stream [in]
+  (let [chunk-size 4096
+        buffer (make-array Byte/TYPE chunk-size)
+        scratch (.read in buffer 0 1800)]
+    (loop [bc (.read in buffer)
+           pos 0
+           matches {}]
+      (if (or (= bc -1) (> pos 10000))
+        [pos matches]
+        (let [fp (fingerprint/fingerprint buffer)
+              ms (get @fingerprint-index fp)
+              offms (map (fn [[ts nm]] [(- ts pos) nm]) ms)
+              newmatches (reduce map-count matches offms)]
+          (if (some #(> % 20) (vals newmatches))
+            [pos newmatches]
+            (recur (.read in buffer)
+                   (inc pos)
+                   newmatches)))))))
