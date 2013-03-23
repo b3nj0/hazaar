c011c8b890f4934169a4de7256a0c3ae35ff6714
Benjamin Jenks <jenksb@gmail.com>
2013-03-23 14:43:41 +0000

    StereoToMonoInputStream: added

--- src/hzr/audio.clj
+++ src/hzr/audio.clj
@@ -1,41 +1,11 @@
 (ns hzr.audio
   (:require [clojure.java.io :as io])
   (:import [javax.sound.sampled AudioSystem AudioInputStream AudioFormat AudioFormat$Encoding DataLine$Info TargetDataLine]
-           [java.io File]))
-
-;; create an input stream that wraps an audio input stream converting
-;; the stream from stereo to mono by averaging the samples on the left
-;; and right channels
-
-(gen-class
-   :name hzr.audio.StereoToMonoInputStream
-   :extends java.io.InputStream
-   :init init
-   :state state
-   :prefix "s2m"
-   :constructors {[InputStream] []})
-
-(defn s2m-init [in]
-  [] {:in in :next (atom -1)})
-
-(defn s2m-read-void [this]
-  (let [s (.state this)
-        next @(:next s)]
-    (if (= -1 next)
-      (let [b0 (.read (:in s))
-            b1 (.read (:in s))
-            b2 (.read (:in s))
-            b3 (.read (:in s))
-            left  (+ (* b0 256) b1)
-            right (+ (* b2 256) b3)
-            mono (/ (+ left right) 2)]
-        (reset! (:next s) (quot mono 256))
-        (mod mono 256))
-      (do (reset! (:next s) -1)
-          next))))
+           [java.io File InputStream]
+           [hzr.audio StereoToMonoInputStream]))
 
 (defn stereo->mono [in]
-  (hzr.audio.StereoToMonoInputStream. in))
+  (StereoToMonoInputStream. in))
 
 (defn- base-to-decoded-format [base-format]
   (let [big-endian true]
