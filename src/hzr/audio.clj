3ba9ce7ee9e1fa69d1cb0d19c2af7bdf020e1fc8
Benjamin Jenks <jenksb@gmail.com>
2013-03-23 12:42:43 +0000

    WIP

--- src/hzr/audio.clj
+++ src/hzr/audio.clj
@@ -3,6 +3,40 @@
   (:import [javax.sound.sampled AudioSystem AudioInputStream AudioFormat AudioFormat$Encoding DataLine$Info TargetDataLine]
            [java.io File]))
 
+;; create an input stream that wraps an audio input stream converting
+;; the stream from stereo to mono by averaging the samples on the left
+;; and right channels
+
+(gen-class
+   :name hzr.audio.StereoToMonoInputStream
+   :extends java.io.InputStream
+   :init init
+   :state state
+   :prefix "s2m"
+   :constructors {[InputStream] []})
+
+(defn s2m-init [in]
+  [] {:in in :next (atom -1)})
+
+(defn s2m-read-void [this]
+  (let [s (.state this)
+        next @(:next s)]
+    (if (= -1 next)
+      (let [b0 (.read (:in s))
+            b1 (.read (:in s))
+            b2 (.read (:in s))
+            b3 (.read (:in s))
+            left  (+ (* b0 256) b1)
+            right (+ (* b2 256) b3)
+            mono (/ (+ left right) 2)]
+        (reset! (:next s) (quot mono 256))
+        (mod mono 256))
+      (do (reset! (:next s) -1)
+          next))))
+
+(defn stereo->mono [in]
+  (hzr.audio.StereoToMonoInputStream. in))
+
 (defn- base-to-decoded-format [base-format]
   (let [big-endian true]
     (AudioFormat. AudioFormat$Encoding/PCM_SIGNED
@@ -15,8 +49,9 @@
 
 (defn decode-audio-file [filename fn]
   (with-open [in (AudioSystem/getAudioInputStream (io/file filename))
-              decoded-in (AudioSystem/getAudioInputStream (base-to-decoded-format (.getFormat in)) in)]
-    (fn decoded-in)))
+              stereo-in (AudioSystem/getAudioInputStream (base-to-decoded-format (.getFormat in)) in)
+              mono-in (stereo->mono stereo-in)]
+    (fn mono-in)))
 
 (defn decoded-audio-file [filename]
   (let [buffer (java.io.ByteArrayOutputStream.)]
